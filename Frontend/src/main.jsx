5a6d608862138bc3112ff5da2c704be1fa298fed
natashagrasso <81648440+natashagrasso@users.noreply.github.com>
2026-02-02 20:40:10 -0300

    Update main.jsx
    Removed commented line about Bootstrap styles.

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-// Importamos los estilos de Bootstrap para que se vea lindo desde el principio
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
